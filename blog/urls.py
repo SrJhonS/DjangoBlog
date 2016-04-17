659a8d57b084656a8bce9a50a0af130fb4269186
SrJhonS <miguel.suarezdefigueroa@hotmail.com>
2016-04-17 10:10:22 +0200

    delete comentado

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
 	url(r'^post/new/$', views.PostCreate.as_view(), name='post_new'), #Nuevo post
 	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'), #Editar post
 	#url(r'^post/(?P<pk>[0-9]+)/delete/$', views.post_delete, name='post_delete'), #Eliminar post
-	url(r'^post/(?P<pk>[0-9]+)/delete/$', views.PostDelete.as_view(), name='post_delete'), #Eliminar post
+	#url(r'^post/(?P<pk>[0-9]+)/delete/$', views.PostDelete.as_view(), name='post_delete'), #Eliminar post
 
 ]
